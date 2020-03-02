51e0d34c60c0aab9b4a5ab86930ff3fea3c2aede
NathanMKruger <nathan.kruger@mymail.champlain.edu>
2020-03-02 10:09:51 -0500

    started WordList

--- Sources/PhoneNumberMnemonics/WordList.swift
+++ Sources/PhoneNumberMnemonics/WordList.swift
@@ -6,3 +6,15 @@ import Foundation
 // YOU FILL IN HERE
 // You'll want to create a mechanism here for loading and querying
 // words.txt. It's up to you how you do this. You may consider a struct.
+struct Words {
+    //var wordList: [String] = []
+    //let wordList : [String] = tempWords.components(separatedBy: "\n")
+    func loadWords() {
+        do {
+            let tempWords = try String(contentsOfFile: "words.txt")
+            let wordList : [String] = tempWords.components(separatedBy: "\n")
+        }   catch {
+            print(error)
+        }
+    }
+}
